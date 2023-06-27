c8e1b2dcf84fa1cc5df1515b547ffa0c9522199a
Heber Marcano <Hebelioo@gmail.com>
2023-06-27 15:52:53 -0400

    added the then function

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,8 +11,25 @@ $.ajax({
     url: forecast,
     method: "GET",
 }) 
-).then()
-// need the day the temperature and humidity wind and description
-
-//I need to put the date the hour and place
-
+).then(function(currentWeatherResponse, forecastResponse) {
+    currentDay = moment(forecastResponse[0].list[0].dt_txt).format("MMMM/Do/YYYY");
+    currentTemp = currentWeatherResponse[0].main.temp;
+    humidity = currentWeatherResponse[0].main.humidity;
+    windSpeed = currentWeatherResponse[0].wind.speed;
+    weatherDesc = currentWeatherResponse[0].weather[0].description;
+    
+    const forecastData = forecastResponse[0].list;
+    const fiveDayForecast = [];
+  for (let i = 7; i < forecastData.lenght; i += 8){
+    const date = moment(forecastData[i].dt_txt).format("MMMM Do, YYYY");
+    const temp = forecastData[i].main.temp;
+    const humidity = forecastData[i].main.humidity;
+    const weatherDesc = forecastData[i].weather[0].description;
+  }
+  const tempF = ((temp - 273.15) * 9/5 + 32).toFixed(2);
+ fiveDayForecast.push({ date, temp: tempF, humidity, weatherDesc });
+}
+function cityweather(){
+    var cit
+}
+)
